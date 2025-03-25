3c6feb47fc93e77b71c576c22d12067aeafc6317
MDelgadoLucas <lucasmariano1011@gmail.com>
2025-03-25 16:05:09 -0300

    Agregado de funciones y print

--- tp1_1.c
+++ tp1_1.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
-int main () {
-    printf("Hola Mundo");
+int main()
+{
+    printf("Hola Mundo \n");
+
+    int num = 10, *p_num;
+    p_num = &num; // asignamos la direccion de memoria de una variable al puntero
+
+    printf("Contenido del puntero: %d \n", *p_num);
+    printf("La direccion de memoria almacenada por el puntero: %p\n", p_num);
+    printf("La direccion de memoria de la variable: %p\n", &num);
+    printf("La direccion de memoria del puntero: %p\n", &p_num);
+    printf("El tamanio de memoria de la variable 'num' es: %zu bytes\n", sizeof(num));
+    printf("El tamanio de memoria del puntero es: %zu bytes\n", sizeof(p_num)); 
+
 
     return 0;
-
 }
